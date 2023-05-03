6a8f10245a4db599d0f6d722143617af1520cd8c
unknown <justice_of_peace@yahoo.com>
2023-05-03 07:57:33 +0530

    redux demo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+// ACTION  --> any function that return value
+const increment=()=>{
+  return {
+    type:'INCREMENT'
+  }
+}
+
+const decrement=()=>{
+  return {
+    type:'DECREMENT'
+  }
+}
+// REDUCER --> manage state of element
+const reducer=(state=0,action)=>{
+  switch (action.type) {
+    case 'INCREMENT':
+      return state+1;
+      break;
+    case 'DECREMENT':
+      return state-1;
+    default:
+      break;
+  }
+
+}
+// STORE--> save the state 
+const store=createStore(reducer);
+store.subscribe(()=>{console.log(store.getState());})
+// DISPATCH --> use to call the action
+store.dispatch(increment());
+store.dispatch(increment())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
